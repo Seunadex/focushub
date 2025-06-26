ab9e20a2ab032662ba74792d4bcc899154b688bf
Seun Adekunle <adekunleseun001@gmail.com>
2025-06-26 19:34:00 +0100

    devise authenthication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get "home/index"
+  devise_for :users
+  # devise_scope :user do
+  #   get "/login", to: "devise/sessions#new"
+  #   get "/signup", to: "devise/registrations#new"
+  #   post "/signup", to: "devise/registrations#create"
+  # end
+  get "/dashboard", to: "dashboard#show", as: :dashboard
+  authenticated :user do
+    root to: "dashboard#show", as: :authenticated_root
+  end
+
+  # Otherwise (guests), show the home landing page
+  unauthenticated do
+    root to: "home#index", as: :unauthenticated_root
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
