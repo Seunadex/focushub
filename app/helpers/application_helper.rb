35e324e6aebb3565083b88fba69dc7ec77920a13
Seun Adekunle <adekunleseun001@gmail.com>
2025-07-02 12:27:52 +0100

    add sidebar and time based greeting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def time_based_greeting
+    hour = Time.zone.now.hour
+
+    case hour
+    when 5..11
+      "Good Morning"
+    when 12..17
+      "Good Afternoon"
+    else
+      "Good Evening"
+    end
+  end
 end
