5cad2e934274a175fe9b3b3828475debfffcb98a
Seun Adekunle <adekunleseun001@gmail.com>
2025-07-02 22:02:01 +0100

    add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tasks, dependent: :destroy
+  has_one_attached :avatar
 
   validates_uniqueness_of :email, case_sensitive: false
 
